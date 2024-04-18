f5c72a1f01107c54cde867084b9189b4019d9856
Wagner Keller <wagner.keller@gmail.com>
2024-04-19 00:08:58 +0100

    Fixed repo name and files

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/wkeller/test
+# Install script for directory: /home/wkeller/movietheathermanager
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/wkeller/test/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/wkeller/movietheathermanager/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
---
+++ templates.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "copying templates"
+pwd
+rm -rf ./build/templates/
+cp -rf ./templates/ ./build/templates/
